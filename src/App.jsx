c4f46e57dd3e6f01147c3a594f97cef63e3b295b
Sho Fujiwara <shofujiwara505@gmail.com>
2023-04-28 17:53:40 +0900

    now forecast works

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import { WEATHER_API_KEY, WEATHER_API_URL } from "./Api";
 function App() {
   const handleOnSearchChange = (searchData) => {
     const [lat, long] = searchData.value.split(" ");
-    const city = `{${WEATHER_API_URL}weather?appid=${WEATHER_API_KEY}&lat=${lat}&lon=${long}}`;
+    const weather = `${WEATHER_API_URL}weather?appid=${WEATHER_API_KEY}&lat=${lat}&lon=${long}`;
+    const forecast = `${WEATHER_API_URL}forecast?appid=${WEATHER_API_KEY}&lat=${lat}&lon=${long}`;
+    console.log(forecast);
   };
   return (
     <>
