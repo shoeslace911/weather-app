4f26851f9dadfde5f4a8f064a95c313c3e42009c
Sho Fujiwara <shofujiwara505@gmail.com>
2023-04-28 17:55:59 +0900

    time to get the data 8)

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ import { WEATHER_API_KEY, WEATHER_API_URL } from "./Api";
 function App() {
   const handleOnSearchChange = (searchData) => {
     const [lat, long] = searchData.value.split(" ");
-    const weather = `${WEATHER_API_URL}weather?appid=${WEATHER_API_KEY}&lat=${lat}&lon=${long}`;
-    const forecast = `${WEATHER_API_URL}forecast?appid=${WEATHER_API_KEY}&lat=${lat}&lon=${long}`;
-    console.log(forecast);
+    const weatherFetch = `${WEATHER_API_URL}weather?appid=${WEATHER_API_KEY}&lat=${lat}&lon=${long}`;
+    const forecastFetch = `${WEATHER_API_URL}forecast?appid=${WEATHER_API_KEY}&lat=${lat}&lon=${long}`;
   };
+  // you were going to fetch from weather and forecast
   return (
     <>
       <h1>Cheese</h1>
