d3b25430d0c7367c727fb9445ebf3b774f829174
Sho Fujiwara <shofujiwara505@gmail.com>
2023-04-30 15:44:40 +0900

    can now show the forecast, but need more polish

--- src/comp/Forecast.jsx
+++ src/comp/Forecast.jsx
@@ -1,15 +1,47 @@
 import PropTypes from "prop-types";
+import { useEffect, useState } from "react";
 
 export default function Forecast({ data }) {
-  // console.log("forecast", data);
-  console.log(data.list[0].dt_txt.split(" "));
-  let [date, time] = data.list[0].dt_txt.split(" ");
-  // let time =
+  let [dateAndTimes, setDateAndTimes] = useState([]);
+
+  useEffect(() => {
+    let extractedDates = data.list;
+    let formattedDateAndTimes = [];
+    extractedDates.map((extractedDate) => {
+      // console.log(extractedDate);
+      // format date to look nice
+      const dateString = extractedDate.dt_txt.split(" ")[0];
+      const date = new Date(dateString);
+      const month = date.getMonth() + 1;
+      const day = date.getDate();
+      const year = date.getFullYear();
+      const formattedDate = `${month}/${day}/${year}`;
+
+      // format time to look nice
+      let formattedTime = extractedDate.dt_txt.split(" ")[1].slice(0, 5);
+
+      if (formattedTime !== "00:00") {
+        formattedDateAndTimes.push({
+          date: formattedDate,
+          time: formattedTime,
+          key: crypto.randomUUID(),
+        });
+      }
+    });
+
+    setDateAndTimes(formattedDateAndTimes);
+  }, []);
   return (
+    // loop over until time is 0000
+
     <div>
-      <h1>Today&apos;s forecast</h1>
-      <p>{date}</p>
-      <p>{time}</p>
+      <h1>4 day forecast</h1>
+      {dateAndTimes.map((dateAndTime) => (
+        <div key={dateAndTime.key}>
+          <p>Date: {dateAndTime.date}</p>
+          <p>Time: {dateAndTime.time}</p>
+        </div>
+      ))}
     </div>
   );
 }
