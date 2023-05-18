c79744166b6cae53830747b04280bd2e2a89fdc1
Sho Fujiwara <shofujiwara505@gmail.com>
2023-05-18 12:16:02 +0900

    forecast shows up

--- src/comp/Forecast.jsx
+++ src/comp/Forecast.jsx
@@ -4,7 +4,6 @@ import { Slider } from "@mui/material";
 
 export default function Forecast({ data }) {
   let [dateAndTimes, setDateAndTimes] = useState([]);
-
   useEffect(() => {
     let extractedDates = data.list;
     let formattedDateAndTimes = [];
@@ -38,8 +37,10 @@ export default function Forecast({ data }) {
       const year = date.getFullYear();
       const dayOfWeek = daysOfWeek[date.getDay()];
       const formattedDate = `${dayOfWeek}, ${month} ${day} ${year}`;
+
       // format time to look nice
       let formattedTime = extractedDate.dt_txt.split(" ")[1].slice(0, 5);
+
       // weather and temp
       let weather = extractedDate.weather[0].main;
       let floorFloat = (float) => {
@@ -56,6 +57,7 @@ export default function Forecast({ data }) {
             feelsLike: [feelsLikeArray],
             icon: [iconArray],
             selectedIndex: 0,
+            isActive: null,
           });
         }
         timeArray = [];
@@ -89,12 +91,25 @@ export default function Forecast({ data }) {
     });
     setDateAndTimes(updatedArray);
   };
+
+  const showForecast = (id) => {
+    const newData = dateAndTimes.map((item) => {
+      if (item.id == id) {
+        return { ...item, isActive: true };
+      }
+      return { ...item, isActive: false };
+    });
+    setDateAndTimes(newData);
+  };
   return (
     <div className="flex pb-96 justify-around my-8">
       {dateAndTimes.map((dateAndTime) => (
         <div
           key={dateAndTime.id}
-          className="border-1 rounded-lg bg-white/30 backdrop-blur-sm my-10 px-2 py-3.5 relative"
+          className="border-1 cursor-pointer rounded-lg bg-white/30 backdrop-blur-sm my-10 px-2 py-3.5 relative"
+          onClick={() => {
+            showForecast(dateAndTime.id);
+          }}
         >
           {/* change depending if dateAndTimes key == this key */}
           <div className="flex">
@@ -104,7 +119,7 @@ export default function Forecast({ data }) {
               <p>Temperature: {dateAndTime.temp[0][dateAndTime.selectedIndex]}</p>
             </div>
           </div>
-          <div className="hidden">
+          <div className={dateAndTime.isActive ? "inline" : "hidden"}>
             <p>Time: {dateAndTime.time[0][dateAndTime.selectedIndex]}</p>
             <p>Weather: {dateAndTime.weather[0][dateAndTime.selectedIndex]}</p>
             <p>Feels like: {dateAndTime.feelsLike[0][dateAndTime.selectedIndex]}</p>
